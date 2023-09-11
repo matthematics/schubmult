841933644bdfc9408eb668e2cedc5ad238139193
Matt Samuel <matthematics@gmail.com>
2023-09-11 14:39:42 -0400

    Add long description

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(
     name="schubmult",
     version="1.0.0",
     description="Computing Littlewood-Richardson coefficients of Schubert polynomials",
+	long_description="""This package is primarily intended for installing the executables schubmult_py, schubmult_yz, and schubmult_double.
+	It exposes some permutation functions as well as three versions of the 'schubmult' function, which are not currently intended for public use.""",
     url="https://github.com/matthematics/schubmult",
     author="Matt Samuel",
 #    author_email="info@realpython.com",
