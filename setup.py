046cff9973f69ee90b80da8efe86107409a3f600
Matt Samuel <matthematics@gmail.com>
2023-11-09 14:21:25 -0500

    Version bump 1.1.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="schubmult",
-    version="1.1.3",
+    version="1.1.4",
     description="Computing Littlewood-Richardson coefficients of Schubert polynomials",
 	long_description=long_description,
 	long_description_content_type='text/markdown',
