377119cebe151e303dffdca7f731a8bfe0ad7c74
Matt Samuel <matthematics@gmail.com>
2025-03-16 19:51:22 -0400

    Lint

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 
 from pathlib import Path
+
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
@@ -8,8 +9,8 @@ setup(
     name="schubmult",
     version="1.5.0",
     description="Computing Littlewood-Richardson coefficients of Schubert polynomials",
-	long_description=long_description,
-	long_description_content_type='text/markdown',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/matthematics/schubmult",
     author="Matt Samuel",
     author_email="schubmult@gmail.com",
@@ -21,19 +22,22 @@ setup(
     include_package_data=True,
     install_requires=[
         "numpy",
-		"PuLP>=2.7.0",
-		"symengine>=0.14.0",
-		"sympy>=1.12",
-		"psutil",
-		"cachetools",
-		"sortedcontainers",
-		"sagemath==1.3.0"
+        "PuLP>=2.7.0",
+        "symengine>=0.14.0",
+        "sympy>=1.12",
+        "psutil",
+        "cachetools",
+        "sortedcontainers",
+        "sagemath==1.3.0",
     ],
-    entry_points={"console_scripts": ["schubmult_py=schubmult.schubmult_py.__main__:main",
-	"schubmult_double=schubmult.schubmult_double.__main__:main",
-	"schubmult_yz=schubmult.schubmult_yz.__main__:main",
-	"schubmult_q=schubmult.schubmult_q.__main__:main",
-	"schubmult_q_double=schubmult.schubmult_q_double.__main__:main",
-	"schubmult_q_yz=schubmult.schubmult_q_yz.__main__:main"
-	]},
+    entry_points={
+        "console_scripts": [
+            "schubmult_py=schubmult.schubmult_py.__main__:main",
+            "schubmult_double=schubmult.schubmult_double.__main__:main",
+            "schubmult_yz=schubmult.schubmult_yz.__main__:main",
+            "schubmult_q=schubmult.schubmult_q.__main__:main",
+            "schubmult_q_double=schubmult.schubmult_q_double.__main__:main",
+            "schubmult_q_yz=schubmult.schubmult_q_yz.__main__:main",
+        ]
+    },
 )
