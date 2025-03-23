03647c1ba4a5a8c2d53d44b7b1d5d116096e0201
Matt Samuel <matthematics@gmail.com>
2025-03-23 08:38:13 -0400

    Fix coprod double tests

--- src/tests/script_tests/test_schubmult_double.py
+++ src/tests/script_tests/test_schubmult_double.py
@@ -20,19 +20,25 @@ def assert_dict_good(v_tuple, input_dict, ret_dict, coprod, indices, same=True):
     else:
         coeff_dict = schubmult(input_dict, v_tuple, var2=var_a, var3=var_a if same else var_b)
     
+    #print(f"{coeff_dict=}")
+    #print(f"{ret_dict=}")
+
     for k, v in coeff_dict.items():
         v = expand(v)
-        if v != 0:
-            assert k in ret_dict
+        if v != 0:            
+            print(f"{k=} {v=}")
             assert v == expand(ret_dict[k])
         else:            
-            assert k not in ret_dict
+            assert k not in ret_dict or v == expand(ret_dict[k])
     for k in ret_dict.keys():
         assert expand(coeff_dict[k]) == expand(ret_dict[k])
 
 def test_coeff_equal_exec(capsys):    
+    import re
     json_files = ["schubmult_double_cf1", 
-                  "schubmult_double_cf2"]
+                  "schubmult_double_cf2",
+                  "schubmult_double_coprod"
+                  ]
     for json_file in json_files:
         from ast import literal_eval
         from schubmult.perm_lib import uncode, permtrim    
@@ -57,6 +63,7 @@ def test_coeff_equal_exec(capsys):
                 try:
                     k, v = line.lstrip().split("  ")
                 except ValueError:
+                    print(f"{line=}")
                     continue
                 try:
                     v = sympify(v)
@@ -64,24 +71,29 @@ def test_coeff_equal_exec(capsys):
                     raise
                 ret_dict[(tuple(literal_eval(k)) if not ascode else tuple(uncode(literal_eval(k))))] = v
         else:
+            nf = 0
             for line in lines:
                 line = str(line)
-                first_split = ") (" if not ascode else "] ["
-                second_split = " (" if not ascode else " ["
-                jn = "),(" if not ascode else "],["
+                charo = "[" if ascode else "("
+                charc = "]" if ascode else ")"
+                first_split = "\\) +\\(" if not ascode else "\\] +\\["
+                second_split = ")  " if not ascode else "]  "
+                jn = ")," if not ascode else "],"
                 try:
-                    vf, s = line.split(first_split)
-                    v, f = vf.split(second_split)
-                    k1, k2 = literal_eval(f"({second_split}{f+jn+s})")
+                    s, vf = re.split(first_split, line)
+                    f, v = vf.split(second_split)# re.split(second_split, vf)
+                    evlaf = f"({charo}{f+jn+s}{charc})"
+                    k1, k2 = literal_eval(evlaf)
                     if ascode:
                         k1 = tuple(permtrim(uncode(k1)))
                         k2 = tuple(permtrim(uncode(k2)))
-                    k = (k1, k2)
+                    k = (k2, k1)
                 except ValueError:
+                    print(f"{line=}")
+                    nf+=1
                     continue
                 v = sympify(v)
                 ret_dict[k] = v
-
             v_tuple = tuple(input_data["v_tuple"])
             assert_dict_good(v_tuple, input_dict, ret_dict, coprod, indices, same)
 
