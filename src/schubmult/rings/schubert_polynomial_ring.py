d4c176bc7fe7b0cc4b56f3510a2c870c9a5c486b
Matt Samuel <matthematics@gmail.com>
2025-03-26 07:12:10 -0400

    Repr

--- src/schubmult/rings/schubert_polynomial_ring.py
+++ src/schubmult/rings/schubert_polynomial_ring.py
@@ -15,35 +15,23 @@ class DictAlgebraElement:
     def __init__(self, _dict = None,  base_var = "x"):
         self._dict = _dict
         self._base_var = base_var
-        self._gens = poly_ring(self._base_var)
+        # self._base_var = poly_ring(self._base_var)
 
-    def __call__(self, x):
-        if isinstance(x, list) or isinstance(x, tuple):
-            # checking the input to avoid symmetrica crashing Sage, see trac 12924
-            elem = DictAlgebraElement({tuple(permtrim(list(x))): 1}, tuple(symarray("x", 100).tolist()))
-        else:
-            result = mult_poly(
-                {(1, 2): 1},
-                sympify(x),
-                poly_ring("x")
-            )
-            elem = DictAlgebraElement(result, tuple(symarray("x", 100).tolist()))        
-        return elem
 
     def __add__(self, other):
         if isinstance(other, DictAlgebraElement):
-            return DictAlgebraElement(add_perm_dict(self._dict, other._dict), self._gens)
+            return DictAlgebraElement(add_perm_dict(self._dict, other._dict), self._base_var)
         else:
-            return DictAlgebraElement(add_perm_dict(self._dict, self(other)._dict), self._gens)
+            return DictAlgebraElement(add_perm_dict(self._dict, self(other)._dict), self._base_var)
 
     def __sub__(self, other):
         if isinstance(other, DictAlgebraElement):
-            return self.__add__(DictAlgebraElement({k: -v for k, v in other._dict.items()}, self._gens))
+            return self.__add__(DictAlgebraElement({k: -v for k, v in other._dict.items()}, self._base_var))
         else:
-            return self.__add__(DictAlgebraElement({k: -v for k, v in self(other)._dict.items()}, self._gens))
+            return self.__add__(DictAlgebraElement({k: -v for k, v in self(other)._dict.items()}, self._base_var))
 
     def __neg__(self):
-        return DictAlgebraElement({k: -v for k, v in self._dict.items()}, self._gens)
+        return DictAlgebraElement({k: -v for k, v in self._dict.items()}, self._base_var)
 
     def _sympify_(self):
         raise TypeError("You cannot sympify this")
@@ -56,18 +44,35 @@ class DictAlgebraElement:
         ret = {}
         for k, v in elem._dict.items():
             ret = add_perm_dict(ret, schubmult({k0: v0 * v for k0, v0 in self._dict.items()}, k))
-        return DictAlgebraElement(ret, self._gens)
+        return DictAlgebraElement(ret, self._base_var)
 
     def __str__(self):
         pieces = []
         for k, v in self._dict.items():
             if expand(v) != 0:
-                pieces += [v * Symbol(f"S({list(k)})")]
+                pieces += [v * Symbol(f"S{self._base_var}({list(k)})")]
         return str(Add(*pieces))
 
     def __repr__(self):
         return self.__str__()
 
 
-Schub = DictAlgebraElement()
+class DictAlgebraElement_basis:
+    def __init__(self, base_var = "x"):
+        self._base_var = base_var
+
+    def __call__(self, x):
+        if isinstance(x, list) or isinstance(x, tuple):
+            # checking the input to avoid symmetrica crashing Sage, see trac 12924
+            elem = DictAlgebraElement({tuple(permtrim(list(x))): 1}, tuple(symarray("x", 100).tolist()))
+        else:
+            result = mult_poly(
+                {(1, 2): 1},
+                sympify(x),
+                poly_ring(self._base_var),
+            )
+            elem = DictAlgebraElement(result, tuple(symarray("x", 100).tolist()))
+        return elem
+
+Schub = DictAlgebraElement_basis()
 SchubertPolynomial = DictAlgebraElement
