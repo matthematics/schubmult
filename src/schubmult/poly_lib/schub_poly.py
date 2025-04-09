de090207ac4d1a2048b97339a69580e35caf8bad
Matt Samuel <matthematics@gmail.com>
2025-04-08 21:24:25 -0400

    schub_poly_from_elem, towards arbitrary basis change

--- src/schubmult/poly_lib/schub_poly.py
+++ src/schubmult/poly_lib/schub_poly.py
@@ -1,6 +1,8 @@
 import sympy
 
+import schubmult.perm_lib.perm_lib as pl
 import schubmult.schub_lib.schub_lib as schub_lib
+from schubmult.poly_lib.poly_lib import call_zvars
 from schubmult.poly_lib.variables import GeneratingSet
 
 
@@ -13,6 +15,54 @@ def div_diff(i, poly, var2=None):
     return sympy.sympify(
         sympy.div(sympy.sympify(poly - perm_act(poly, i)), sympy.sympify(var2[i] - var2[i + 1]))[0],
     )
+
+
+def elem_func_func(k, i, v1, v2, vdiff, varl1, varl2, elem_func):
+    newk = k
+    if newk < vdiff:
+        return 0
+    if newk == vdiff:
+        return 1
+    zvars = [varl2[i] for i in call_zvars(v1, v2, k, i)]
+    return elem_func(newk - vdiff, newk, varl1[1:], zvars)
+
+
+def schubpoly_from_elems(v, var_x=None, var_y=None, elem_func=None):
+    th = pl.strict_theta(~pl.Permutation(v))
+    mu = pl.uncode(th)
+    vmu = pl.Permutation(v) * mu  # permtrim(mulperm([*v], mu))
+    if len(th) == 0:
+        return elem_func(0, 0, var_x, var_y)
+    while len(th) > 0 and th[-1] == 0:
+        th.pop()
+    vpathdicts = schub_lib.compute_vpathdicts(th, vmu)
+    vpathsums = {pl.Permutation([1, 2]): elem_func(0, 0, var_x, var_y)}
+    for index in range(len(th)):
+        mx_th = 0
+        newpathsums = {}
+        for vp in vpathdicts[index]:
+            for v2, vdiff, s in vpathdicts[index][vp]:
+                mx_th = max(mx_th, th[index] - vdiff)
+        for v in vpathdicts[index]:
+            sumval = vpathsums.get(v, 0)
+            if sumval == 0:
+                continue
+            for v2, vdiff, s in vpathdicts[index][v]:
+                newpathsums[v2] = newpathsums.get(
+                    v2,
+                    0,
+                ) + s * sumval * elem_func_func(
+                    th[index],
+                    index + 1,
+                    v,
+                    v2,
+                    vdiff,
+                    var_x,
+                    var_y,
+                    elem_func=elem_func,
+                )
+        vpathsums = newpathsums
+    return vpathsums.get(vmu, 0)
 
 
 def schubpoly(v, var2=GeneratingSet("y"), var3=GeneratingSet("z"), start_var=1):
