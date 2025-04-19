612334aba6a59c1c430389f921223eb4d4b92ac1
Matt Samuel <matthematics@gmail.com>
2025-04-18 23:16:28 -0400

    Front facing schubmult import

--- src/schubmult/__init__.py
+++ src/schubmult/__init__.py
@@ -1,11 +1,12 @@
 __version__ = "3.0.dev"
-
+#schubpoly_from_elems?
 from .perm_lib import Permutation
 from .poly_lib.poly_lib import efficient_subs, elem_sym_poly, elem_sym_poly_q, q_vector, xreplace_genvars
 from .poly_lib.schub_poly import div_diff, schubpoly, skew_div_diff
 from .poly_lib.variables import CustomGeneratingSet, GeneratingSet, GeneratingSet_base, MaskedGeneratingSet
 from .rings._quantum_schubert_polynomial_ring import (
     QDSx,
+    QPDSx,
     QSx,
 )
 from .rings._schubert_polynomial_ring import (
@@ -22,7 +23,15 @@ from .schub_lib.double import (
     schubmult_generic_partial_posify,
 )
 from .schub_lib.quantum import mult_poly_q, schubmult_q, schubmult_q_fast
-from .schub_lib.quantum_double import factor_out_q_keep_factored, mult_poly_q_double, schubmult_q_double, schubmult_q_double_fast, schubpoly_quantum
+from .schub_lib.quantum_double import (
+    factor_out_q_keep_factored,
+    mult_poly_q_double,
+    schubmult_q_double,
+    schubmult_q_double_fast,
+    schubmult_q_double_pair_generic,
+    schubmult_q_generic_partial_posify,
+    schubpoly_quantum,
+)
 from .schub_lib.single import (
     mult_poly_py,
     schub_coprod_py,
@@ -37,6 +46,7 @@ __all__ = [
     "MaskedGeneratingSet",
     "Permutation",
     "QDSx",
+    "QPDSx",
     "QSx",
     "Sx",
     "compute_positive_rep",
@@ -50,6 +60,8 @@ __all__ = [
     "mult_poly_q",
     "mult_poly_q_double",
     "q_vector",
+    "rings",
+    "sage",
     "schub_coprod_double",
     "schub_coprod_py",
     "schubmult_double",
@@ -60,7 +72,9 @@ __all__ = [
     "schubmult_q",
     "schubmult_q_double",
     "schubmult_q_double_fast",
+    "schubmult_q_double_pair_generic",
     "schubmult_q_fast",
+    "schubmult_q_generic_partial_posify",
     "schubpoly",
     "schubpoly_quantum",
     "skew_div_diff",
