0b1741f8fc3b5f25b5cfe80ad53cdf54c106b154
Matt Samuel <matthematics@gmail.com>
2025-04-30 22:18:51 -0400

    Add e = ElemSym to abc

--- src/schubmult/abc.py
+++ src/schubmult/abc.py
@@ -1,6 +1,7 @@
 from schubmult.poly_lib.variables import GeneratingSet
+from schubmult.rings.elem_sym import ElemSym
 
+e = ElemSym
 x = GeneratingSet("x")
 y = GeneratingSet("y")
 z = GeneratingSet("z")
-
