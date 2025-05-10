2df728fa5ebcd3e9a3acb9a363ab509351fe2d48
Matt Samuel <matthematics@gmail.com>
2025-05-10 08:30:03 -0400

    abc more vars

--- src/schubmult/abc.py
+++ src/schubmult/abc.py
@@ -1,6 +1,13 @@
+from symengine import var
+
 from schubmult.rings.symmetric_polynomials import CompleteSym, E, ElemSym, FactorialCompleteSym, FactorialElemSym, H, e, h  # noqa: F401
 from schubmult.rings.variables import GeneratingSet
+
+var("x_(1:100)")
+var("y_(1:100)")
+var("z_(1:100)")
 
 x = GeneratingSet("x")
 y = GeneratingSet("y")
 z = GeneratingSet("z")
+
