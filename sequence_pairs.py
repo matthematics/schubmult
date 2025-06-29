7ecbf15a1c87e62a23572c2c0e7b88c323319ec8
Matt Samuel <matthematics@gmail.com>
2025-06-29 09:54:53 -0400

    GRASS COPROD

--- sequence_pairs.py
+++ sequence_pairs.py
@@ -1,5 +1,6 @@
 from schubmult import *
 from schubmult.schub_lib.schub_lib import pull_out_var
+from schubmult.rings import *
 from schubmult.symbolic import *
 from schubmult.utils.perm_utils import mu_A
 from itertools import permutations
@@ -34,8 +35,8 @@ def sep_desc_mul(elem, perm2):
     pmu2 = (~perm2).minimal_dominant_above()
     mu2 = pmu2.code
     res = S.Zero
-    mul2 = DSx(perm2*pmu2)
-    for perm in elem:
+    mul2 = elem.ring.from_dict({perm2*pmu2: S.One})
+    for perm, v2 in elem.items():
         pmu1 = (~perm).minimal_dominant_above()
         mu1 = pmu1.code
         if len(mu1) > 0:
@@ -49,7 +50,7 @@ def sep_desc_mul(elem, perm2):
                 bigmu += [mu2[i]]
         pmu1 = uncode(mu1)
         pmu = uncode(bigmu)
-        bingo = DSx(perm*pmu1) * mul2
+        bingo = mul2.ring.from_dict({perm*pmu1: v2}) * mul2
         dct = {}
         ipmu = ~pmu
         for w, v in bingo.items():
@@ -57,6 +58,16 @@ def sep_desc_mul(elem, perm2):
         res += bingo.ring.from_dict(dct)
     return res
 
+def h_coprod(p, k):
+    ret = []
+    for i in range(p + 1):
+        if i == 0:
+            ret += [[[], ([0] * (k-1)) + [ p -i ]]]
+        elif i == p:
+            ret += [[([0] * (k-1)) + [ p ], []]]
+        else:
+            ret += [[([0] * (k-1)) + [ i], ([0] * (k-1)) + [p - i]]]
+    return ret
     # for perm2 in perms:
     #     mu1 = (~perm).minimal_dominant_above().code
     #     mu2 = (~perm2).minimal_dominant_above().code
@@ -78,12 +89,39 @@ def sep_desc_mul(elem, perm2):
     #         dct[w*(~mu)] = v
     #     print(f"{perm}*{perm2} = {bingo.ring.from_dict(dct)}")
 
-pows = [[2], [1], [0, 2]]
+  # pows = [[0, 0, 1], [3], [5]]
+pows = [[1, 3], [2, 1]]
 
-res = DSx([])
+
+res = Sx([])
+
+RR = TensorRing(res.ring, res.ring)
+
+elems = []
+
+
+for pork in pows:
+    dct = {}
+    for panko, fong in h_coprod(*pork):
+        dct[(uncode(panko), uncode(fong))] = S.One
+    elems += [RR.from_dict(dct)]
+
 Permutation.print_as_code = True
-for cd in pows:
-    res = sep_desc_mul(res, uncode(cd))
+res = RR.one
+from schubmult.rings._mul_utils import _tensor_product_of_dicts, add_perm_dict
+
+for elem in elems:
+    ret = S.Zero
+    for k1, v1 in res.items():
+        for k2, v2 in elem.items():
+            dct = sep_desc_mul(RR.rings[0].from_dict({k1[0]: v1 * v2}), k2[0])
+            dct2 = sep_desc_mul(RR.rings[1].from_dict({k1[1]: 1}),k2[1])
+            dct = _tensor_product_of_dicts(dct, dct2)
+            ret += RR.from_dict(dct)
+    res = ret
     print(res)
+# for cd in pows:
+#     res = sep_desc_mul(res, uncode(cd))
+#     print(res.expand(deep=False))
 
         
