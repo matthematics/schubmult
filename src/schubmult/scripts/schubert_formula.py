16c0f5a2c4937723f3f72517a970cbac105842b9
Matt Samuel <matthematics@gmail.com>
2025-09-10 17:13:18 -0400

    FASTER

--- src/schubmult/scripts/schubert_formula.py
+++ src/schubmult/scripts/schubert_formula.py
@@ -1,6 +1,6 @@
 import sys
 
-from schubmult import ASx, uncode
+from schubmult import ASx, Permutation, uncode
 from schubmult.rings import FA, Sx, WordBasis
 from schubmult.rings.rc_graph_module import RCGraph, RCGraphModule, RCGraphTensor, TensorModule
 from schubmult.utils.perm_utils import artin_sequences
@@ -29,39 +29,37 @@ def main():
 
 
     # this is an inner product of RCs
-    for aseq1 in aseqs:
-        posmod = FA(*aseq1) * unit_rc_module
-        for rc4, coeff4 in posmod.items():
-            assert coeff4 == 1
+    perms = Permutation.all_permutations(n)
+    for perm in perms:
+        perm_words = ASx(perm, n - 1).change_basis(WordBasis)
+        # mod = ASx(perm, n - 1) * unit_rc_module
+        for rc4 in RCGraph.all_rc_graphs(perm, n-1):
             magic_coeffs = {}
-            for aseq0 in aseqs:
-                perm = uncode(aseq0)
-                perm_words = ASx(perm, n - 1).change_basis(WordBasis)
-                mod = ASx(perm, n - 1) * unit_rc_module
-                multiplier = 0
-                for rc3, coeff3 in mod.items(): #mod is ASx rc graph module, coeff3 ia an ASx perm coeff
-                    if len(rc3.perm) > n:
+            # PUT THIS BACK IF NEEDED
+            # multiplier = 0
+            # for rc3, coeff3 in mod.items(): #mod is ASx rc graph module, coeff3 ia an ASx perm coeff
+            #     if len(rc3.perm) > n:
+            #         continue
+            #     if rc3.perm != rc4.perm:
+            #         continue
+            #     multiplier += coeff3
+            # assert multiplier == 1
+            # this inner products it
+            # sumtiplier = 0
+            for aseq, coeff, in perm_words.items(): # ASx perm word coeff is coeff
+                mod2 = FA(*aseq).coproduct() * unit_tensor_rc_module
+                # coeff is perm_words for some coeff3
+                for (rc1, rc2), coeff1 in mod2.items(): # positive coproduct coeff is coeff1
+                    if len(rc1.perm) > n or len(rc2.perm) > n:
                         continue
-                    if rc3.perm != rc4.perm:
-                        continue
-                    multiplier += coeff3
-                # this inner products it
-                sumtiplier = 0
-                for aseq, coeff, in perm_words.items(): # ASx perm word coeff is coeff
-                    mod2 = FA(*aseq).coproduct() * unit_tensor_rc_module
-                    # coeff is perm_words for some coeff3
-                    for (rc1, rc2), coeff1 in mod2.items(): # positive coproduct coeff is coeff1
-                        if len(rc1.perm) > n or len(rc2.perm) > n:
-                            continue
-            
-                        perm1, perm2 = rc1.perm, rc2.perm
-                        magic_coeffs[(perm1, perm2)] = magic_coeffs.get((perm1,perm2),0) + multiplier * coeff * coeff1 
-                        # print(f"{perm1.trimcode} {perm2.trimcode} += {multiplier*coeff*coeff1}")
-                        # print(RCGraphTensor(rc1, rc2))
-                        # print("inner product")
-                        # print(rc4)
-                        sumtiplier += multiplier * coeff
-                assert sumtiplier == 0 or rc4.perm == perm
+
+                    perm1, perm2 = rc1.perm, rc2.perm
+                    magic_coeffs[(perm1, perm2)] = magic_coeffs.get((perm1,perm2),0) + coeff * coeff1 
+                    # print(f"{perm1.trimcode} {perm2.trimcode} += {multiplier*coeff*coeff1}")
+                    # print(RCGraphTensor(rc1, rc2))
+                    # print("inner product")
+                    # print(rc4)
+                    #sumtiplier += multiplier * coeff
 
             for key, magic_coeff in magic_coeffs.items():
                 assert magic_coeff >= 0
