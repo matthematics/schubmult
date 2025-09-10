0c58570a0cb9addc7da5963077683946c14c739c
Matt Samuel <matthematics@gmail.com>
2025-09-10 18:45:22 -0400

    Sum it up two ways

--- src/schubmult/scripts/schubert_formula.py
+++ src/schubmult/scripts/schubert_formula.py
@@ -1,6 +1,7 @@
 import sys
 
 from schubmult import ASx, Permutation, uncode
+from schubmult.abc import x
 from schubmult.rings import FA, Sx, WordBasis
 from schubmult.rings.rc_graph_module import RCGraph, RCGraphModule, TensorModule
 
@@ -25,17 +26,22 @@ def main():
     unit_tensor_rc_module = TensorModule.ext_multiply(unit_rc_module, unit_rc_module)
 
     perm_modules3 = {}
+    perm_modules4 = {}
 
     # this is an inner product of RCs
     perms = Permutation.all_permutations(n)
     # additionally extract the coefficient of a specific rc graph by length vector
+    checks = {}
     for perm in perms:
         perm_words = ASx(perm, n - 1).change_basis(WordBasis)
         # mod = ASx(perm, n - 1) * unit_rc_module
-        alternative_magic_coeffs = {}
+        
+        
         for rc4 in RCGraph.all_rc_graphs(perm, n - 1):
             extra_coeff = perm_words.get(rc4.length_vector(), 0)
+            alternative_magic_coeffs = {}
             magic_coeffs = {}
+            
 
             # PUT THIS BACK IF NEEDED
             # multiplier = 0
@@ -58,18 +64,24 @@ def main():
             ) in perm_words.items():  # ASx perm word coeff is coeff
                 mod2 = FA(*aseq).coproduct() * unit_tensor_rc_module
                 # coeff is perm_words for some coeff3
+                do_sum = False
+                if rc4.length_vector() == aseq:
+                    do_sum = True
                 for (rc1, rc2), coeff1 in mod2.items():  # positive coproduct coeff is coeff1
                     if len(rc1.perm) > n or len(rc2.perm) > n:
                         continue
                     assert coeff1 == 1
                     perm1, perm2 = rc1.perm, rc2.perm
                     magic_coeffs[(perm1, perm2)] = magic_coeffs.get((perm1, perm2), 0) + coeff * coeff1
-                    alternative_magic_coeffs[(perm1, perm2, perm, rc4.length_vector())] = (
-                        alternative_magic_coeffs.get((perm1, perm2, perm, rc4.length_vector()), 0)
-                        + coeff
-                        * coeff1
-                        * extra_coeff
-                    )
+                    if do_sum:
+                        alternative_magic_coeffs[(perm1, perm2)] = (
+                            alternative_magic_coeffs.get((perm1, perm2), 0)
+                            + coeff
+                            * coeff1
+                            * extra_coeff
+                        )
+                # if do_sum:
+                #     checks[(perm, aseq)] = checks.get((perm, aseq), 0) + coeff * extra_coeff
                     # print(f"{perm1.trimcode} {perm2.trimcode} += {multiplier*coeff*coeff1}")
                     # print(RCGraphTensor(rc1, rc2))
                     # print("inner product")
@@ -81,8 +93,11 @@ def main():
                 #assert alternative_magic_coeffs.get((*key,rc4.length_vector()), 0) == magic_coeff * 
                 perm_modules3[key] = perm_modules3.get(key, RCGraphModule()) + magic_coeff * rc4
 
-    for (perm1, perm2, perm, seq), val in alternative_magic_coeffs.items():
-        assert val == sum([v for rc, v in perm_modules3[(perm1, perm2)].items() if rc.length_vector() == seq and rc.perm == perm])
+            for key, amagic_coeff in alternative_magic_coeffs.items():
+                perm_modules4[key] = perm_modules4.get(key, 0) + amagic_coeff * Sx(rc4.polyvalue(x))
+
+    # assert all(v == 1 for v in checks.values()), f"checks failed {checks}"
+
 
     for (perm1, perm2), elem in perm_modules3.items():
         product = Sx(perm1) * Sx(perm2)
@@ -95,6 +110,7 @@ def main():
             assert coeff == product.get(rc.perm, 0)
             val[rc.perm] = coeff
         assert val == product
+        assert perm_modules4.get((perm1, perm2), 0) == product
         print(f"Success {perm1.trimcode} {perm2.trimcode}")
     exit()
 
