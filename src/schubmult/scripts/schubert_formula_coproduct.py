f50f171bee8f718a1166adedd170caecaeef1f13
Matt Samuel <matthematics@gmail.com>
2025-09-11 07:06:50 -0400

    FIFI

--- src/schubmult/scripts/schubert_formula_coproduct.py
+++ src/schubmult/scripts/schubert_formula_coproduct.py
@@ -30,10 +30,11 @@ def main():
     perms = Permutation.all_permutations(n)
     # Act ASx, principals cancel
     aseqs = artin_sequences(n-1)
-    # for seq in aseqs:
     for seq in aseqs:
-        #pish_mod = ASx(perm, n-1) * unit_rc_module
+    #for perm in aseqs:
+
         pish_mod = FA(*seq) * unit_rc_module
+        #pish_mod = FA(*seq) * unit_rc_module
         for rc, bob in pish_mod.items():
             if len(rc.perm) > n:
                 continue
@@ -43,7 +44,9 @@ def main():
             #     if vector_sum(rc1.length_vector(), rc2.length_vector()) == seq:
             #         #print(f"Adding {(rc, (rc1, rc2))} with coeff {bob*coeff*coeff2}")
             #solution_module2 += bob * TensorModule.ext_multiply(pish_mod,ASx(rc.perm,n-1).coproduct()*unit_tensor_rc_module)
-            solution_module2 += bob * TensorModule.ext_multiply(pish_mod,ASx(rc.perm,n-1).coproduct()*unit_tensor_rc_module)
+            # did the whole pish mod!!!!
+            if rc.is_principal:
+                solution_module2 += bob * TensorModule.ext_multiply(pish_mod,FA(*rc.length_vector()).coproduct()*unit_tensor_rc_module)
     # for seq in aseqs:
     #     pish_mod = FA(*seq) * unit_rc_module
     #     for rc, bob in pish_mod.items():
@@ -116,22 +119,23 @@ def main():
     rr = Sx@rr0
     sxt = rr.zero
 
-    for (rc0, (rc1, rc2)), coeff1 in solution_module2.items():
-        if len(rc0.perm) > n or len(rc1.perm) > n or len(rc2.perm) > n:
-            continue
-        #if rc1.is_principal and rc2.is_principal:
-        #coproducts2[rc0.perm] = coproducts2.get(rc0.perm, 0) + coeff1 * tring(((rc1.perm,n-1), (rc2.perm,n-1)))
-        sxt += coeff1*rr.ext_multiply(Sx(rc0.polyvalue(x)),rr0.ext_multiply(Sx(rc1.perm),Sx(rc2.perm)))
-
-    
-
     # for (rc0, (rc1, rc2)), coeff1 in solution_module2.items():
     #     if len(rc0.perm) > n or len(rc1.perm) > n or len(rc2.perm) > n:
     #         continue
+
+        #if rc1.is_principal and rc2.is_principal:
+        #coproducts2[rc0.perm] = coproducts2.get(rc0.perm, 0) + coeff1 * tring(((rc1.perm,n-1), (rc2.perm,n-1)))
+        #sxt += coeff1*rr.ext_multiply(Sx(rc0.perm),rr0.ext_multiply(Sx(rc1.perm),Sx(rc2.perm)))
+
+    
+
+    for (rc0, (rc1, rc2)), coeff1 in solution_module2.items():
+        if len(rc0.perm) > n or len(rc1.perm) > n or len(rc2.perm) > n:
+            continue
         #coproducts2[rc0.perm] = coproducts2.get(rc0.perm, 0) + coeff1 * tring(((rc1.perm,n-1), (rc2.perm,n-1)))
         
         #shoeff = ASx(rc0.perm,n-1).change_basis(SchubertBasis).get(rc0.length_vector(),0)
-        #products[(rc1.perm, rc2.perm)] = products.get((rc1.perm, rc2.perm), 0) + coeff1 * shoeff * Sx(rc0.perm)
+        products[(rc1.perm, rc2.perm)] = products.get((rc1.perm, rc2.perm), 0) + coeff1 *  Sx(rc0.perm)
         #if rc1.is_principal and rc2.is_principal:
             #products[(rc1.perm, rc2.perm)] = products.get((rc1.perm, rc2.perm), RCGraphModule()) + coeff1 * rc0
             #
@@ -158,11 +162,11 @@ def main():
     #     products[(perm2, perm3)] = products.get((perm2, perm3), 0) + coeff * Sx(perm1)
         coproducts2[perm3] = coproducts2.get(perm3, 0) + coeff * (ASx@ASx).ext_multiply(ASx(perm2,n-1),ASx(perm2,n-1))
     
-    # for (perm1, perm2),elem in products.items():
-    #     if any(len(perm) > n for perm in (Sx(perm1) * Sx(perm2)).keys()):
-    #        continue 
-    #     print(perm1.trimcode, perm2.trimcode)
-    #     print(elem)
+    for (perm1, perm2),elem in products.items():
+        if any(len(perm) > n for perm in (Sx(perm1) * Sx(perm2)).keys()):
+           continue 
+        print(perm1.trimcode, perm2.trimcode)
+        print(elem)
 
     #         # check
     #     assert Sx(perm1) * Sx(perm2) == elem, f"Failed check {perm1}, {perm2} {elem=} {Sx(perm1) * Sx(perm2)=}"
