1d0fe2a6c6807dc6cfefc05352dd5cc309f97a1a
Matt Samuel <matthematics@gmail.com>
2025-09-18 12:11:03 -0400

    Testing verification

--- src/schubmult/scripts/nr_verification.py
+++ src/schubmult/scripts/nr_verification.py
@@ -4,6 +4,7 @@ import os
 import shutil
 import sys
 import time
+from itertools import zip_longest
 from json import dump, load
 from multiprocessing import Lock, Manager, Pool, Process, cpu_count
 
@@ -20,7 +21,7 @@ def co_principal_perms(perm):
     return [key[0] for key in upper_elem.keys() if key[0] != perm]
 
 def main():
-    from schubmult import Permutation
+    from schubmult import Permutation, uncode
 
     # try:
     all_rc_graphs = {}
@@ -37,21 +38,31 @@ def main():
     for perm in perms:
         modules = try_lr_module(perm)
         print(all_rc_graphs[perm])
+        all_present_graphs = {}
+        length = 0
         for (rc1, rc2), coeff in modules.items():
+            length = len(rc1)
+
             print(f"{perm.trimcode}: {rc1.perm.trimcode} {len(rc2.perm.trimcode)}")
             print(f"  {latest_seen_left[perm][len(rc1)].get(rc1.perm, -1)} * {latest_seen_right[perm][len(rc2)].get(rc2.perm, -1)} latest seen")
-            
+
             index1 = all_rc_graphs[rc1.perm][len(rc1)][rc1]
             index2 = all_rc_graphs[rc2.perm][len(rc2)][rc2]
+            #all_present_graphs[(rc1.perm, rc2.perm)].update([((rc1, rc2) for rc1 in all_rc_graphs(rc1.perm)[len(rc1)] if rc1 != rc1] ))
             print(f"{index1} * {index2} = {coeff}")
+            #all_pairs = {(rc01, rc02) for rc1, rc2 in product(all_rc_graphs(rc1.perm)[len(rc1)], all_rc_graphs(rc2.perm)[len(rc2)]) if vector_sum(rc1.length_vector(), rc2.length_vector()) == vector_sum(rc01.length_vector(), rc02.length_vector())}
+
+            #all_present_graphs[(rc1.perm, rc2.perm)].update()
             try:
-                assert (latest_seen_left[perm][len(rc1)].get(rc1.perm,-1) < index1) or (latest_seen_right[perm][len(rc2)].get(rc2.perm,-1) < index2)
+                assert (latest_seen_left[perm][len(rc1)].get(rc1.perm,-1),latest_seen_right[perm][len(rc2)].get(rc2.perm,-1)) < (index1, index2)
             except AssertionError:
                 print(f"Order violation: {perm.trimcode}: {rc1.perm.trimcode} {len(rc2.perm.trimcode)}")
                 print("********VIOLATION*******")
                 print(f"  {latest_seen_left[perm][len(rc1)].get(rc1.perm)} * {latest_seen_right[perm][len(rc2)].get(rc2.perm)} latest seen")
                 print(f"  {index1} * {index2}")
                 print("********END VIOLATION*******")
+            all_present_graphs[(rc1.perm, rc2.perm)] = all_present_graphs.get((rc1.perm, rc2.perm), set())
+            all_present_graphs[(rc1.perm, rc2.perm)].add((rc1, rc2))
             #print(f"  {index1} * {index2} = {coeff}")
             # latest_seen_left[perm][len(rc1)][rc1.perm] = index1
             # latest_seen_right[perm][len(rc2)][rc2.perm] = index2
@@ -62,6 +73,12 @@ def main():
                     assert co_perm.trimcode < perm.trimcode
                     latest_seen_left[co_perm][len(rc1)][rc1.perm] = index1 if latest_seen_left[co_perm][len(rc1)].get(rc1.perm, -1) < index1 else latest_seen_left[co_perm][len(rc1)][rc1.perm]
                     latest_seen_right[co_perm][len(rc2)][rc2.perm] = index2 if latest_seen_right[co_perm][len(rc2)].get(rc2.perm, -1) < index2 else latest_seen_right[co_perm][len(rc2)][rc2.perm]
+    
+        for (perm1, perm2), seen_set in all_present_graphs.items():
+            for rc1, index1 in all_rc_graphs[perm1][length].items():
+                for rc2, index2 in all_rc_graphs[perm2][length].items():
+                    if uncode([a+b for a,b in zip_longest(rc1.length_vector(), rc2.length_vector())]) == perm and (latest_seen_left[perm][len(rc1)].get(rc1.perm,-1),latest_seen_right[perm][len(rc2)].get(rc2.perm,-1)) < (index1, index2) and (rc1, rc2) not in seen_set:
+                        print(f"Missing graph for {perm.trimcode}: {rc1.perm.trimcode,rc1.length_vector()} {rc2.perm.trimcode,rc2.length_vector()} at length {length}")
         print("---------")
 
 if __name__ == "__main__":
