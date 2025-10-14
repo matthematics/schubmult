80ca77c522d36f1f9785f73ae4ae08e8184864bc
Matt Samuel <matthematics@gmail.com>
2025-10-14 18:42:51 -0400

    Bije

--- src/schubmult/scripts/toy.py
+++ src/schubmult/scripts/toy.py
@@ -7,8 +7,13 @@ if __name__ == "__main__":
     ring = RCGraphRing()
     n = int(sys.argv[1])
     perms = Permutation.all_permutations(n)
+    tring = ring @ ring
     for perm in perms:
-        elem = ring.from_free_algebra_element(ASx(perm, len(perm.trimcode)))
-        print(elem)
+        cprd = ASx(perm).change_basis(WordBasis).coproduct()
+        elem = tring.zero
+        for (w1, w2), coeff in cprd.items():
+            elem += coeff*tring.ext_multiply(ring.from_free_algebra_element(FA(*w1)),ring.from_free_algebra_element(FA(*w2)))
         pretty_print(elem)
+        # elem = ring.from_free_algebra_element(ASx(perm, len(perm.trimcode)))
+        # pretty_print(elem)
     
