2c7ab5415bac8d52def0d1142db968b2c2c3ea52
Matt Samuel <matthematics@gmail.com>
2025-10-25 09:02:57 -0400

    These tensors look correct

--- src/schubmult/rings/crystal_graph.py
+++ src/schubmult/rings/crystal_graph.py
@@ -20,25 +20,18 @@ class CrystalGraph(Printable):
     def phi(self, i):
         if i < 1 or i >= self.crystal_length():
             return 0
-        if hasattr(self, "_cached_phi") and i in self._cached_phi:
-            return self._cached_phi[i]
-        rc_hw, raise_seq = self.to_highest_weight()
-        top_phi = rc_hw.crystal_weight[i - 1] - rc_hw.crystal_weight[i]
-        for r in reversed(raise_seq):
-            if r == i:
-                top_phi -= 1
-            elif r == i + 1:
-                top_phi += 1
-        if hasattr(self, "_cached_phi"):
-            self._cached_phi[i] = top_phi
-        else:
-            self._cached_phi = {i: top_phi}
-        return top_phi
+        return self.epsilon(i) + self.crystal_weight[i - 1] - self.crystal_weight[i]
 
 
     def epsilon(self, i):
         if i > 0 and i < self.crystal_length():
-            return self.crystal_weight[i - 1] - self.crystal_weight[i] + self.phi(i)
+            cnt = 0
+            rc = self
+            while rc is not None:
+                rc = rc.raising_operator(i)
+                if rc is not None:
+                    cnt += 1
+            return cnt
         return 0
 
     def to_lowest_weight(self, length=None):
@@ -170,18 +163,18 @@ class CrystalGraphTensor(CrystalGraph):
         return max(factor.crystal_length() for factor in self.factors)
 
     def lowering_operator(self, index):
-        if self.epsilon(index) < self.phi(index):
-            tz = CrystalGraphTensor(self.factors[0], self.factors[1].lowering_operator(index))
-            if tz.factors[1] is None:
+        if self.factors[1].epsilon(index) < self.factors[0].phi(index):
+            tz = CrystalGraphTensor(self.factors[0].lowering_operator(index), self.factors[1])
+            if tz.factors[0] is None:
                 return None
             return tz
-        tz = CrystalGraphTensor(self.factors[0].lowering_operator(index), self.factors[1])
-        if tz.factors[0] is None:
+        tz = CrystalGraphTensor(self.factors[0], self.factors[1].lowering_operator(index))
+        if tz.factors[1] is None:
             return None
         return tz
 
     def raising_operator(self, index):
-        if self.epsilon(index) > self.phi(index):
+        if self.factors[1].epsilon(index) <= self.factors[0].phi(index):
             tz = CrystalGraphTensor(self.factors[0].raising_operator(index), self.factors[1])
             if tz.factors[0] is None:
                 return None
@@ -191,8 +184,8 @@ class CrystalGraphTensor(CrystalGraph):
             return None
         return tz
 
-    def phi(self, i):
-        return self.factors[0].phi(i) + max(0, self.factors[1].phi(i) - self.factors[0].epsilon(i))
+    # def phi(self, i):
+    #     return self.factors[0].phi(i) + max(0, self.factors[1].phi(i) - self.factors[0].epsilon(i))
 
-    def epsilon(self, i):
-        return self.factors[1].epsilon(i) + max(0, self.factors[0].epsilon(i) - self.factors[1].phi(i))
+    # def epsilon(self, i):
+    #     return self.factors[1].epsilon(i) + max(0, self.factors[0].epsilon(i) - self.factors[1].phi(i))
--- src/schubmult/scripts/monk_try.py
+++ src/schubmult/scripts/monk_try.py
@@ -9,15 +9,13 @@ if __name__ == "__main__":
 
     perms = Permutation.all_permutations(n)
 
-    dominant_graphs = {RCGraph.principal_rc(Permutation([]).swap(i, i+1),n-1) for i in range(n)}
+    dominant_graphs = {RCGraph.principal_rc(Permutation([]).swap(i, i+1),n-1) for i in range(n - 1)}
 
     for perm in perms:
         hw_rcs = set()
         for rc0 in RCGraph.all_rc_graphs(perm, n-1):
              hw_rcs.add(rc0.to_highest_weight()[0])
         for rc in hw_rcs:
-            if rc in dominant_graphs:
-                continue
             for dom in dominant_graphs:
                 tens = CrystalGraphTensor(rc, dom)
                 hw_set = tens.all_highest_weights()
