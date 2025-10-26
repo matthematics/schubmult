c2b0912d1748c493512ef1db1e964df6ba2389c0
Matt Samuel <matthematics@gmail.com>
2025-10-26 19:44:18 -0400

    LR RULE

--- src/schubmult/scripts/dominant_try.py
+++ src/schubmult/scripts/dominant_try.py
@@ -1,6 +1,6 @@
 
 from schubmult.rings.ck_ring import CoxeterKnuthRing
-
+import itertools
 
 if __name__ == "__main__":
     from schubmult import *
@@ -20,67 +20,45 @@ if __name__ == "__main__":
     for perm in perms:
         if perm.inv == 0:
             continue
-        for dom in dominant_graphs:
-            if dom.perm.inv == 0:
+        result = {}
+        for perm1 in perms:
+            if not perm1.bruhat_leq(perm):
                 continue
-            if not dom.perm.bruhat_leq(perm):
+            dom = RCGraph.principal_rc(Permutation.w0(n-1), n-1)
+            rc_check = perm1 * (~dom.perm)
+            perm_top = (~rc_check) * perm
+            if perm_top.inv != perm.inv + rc_check.inv:
                 continue
-            if max(len(perm0) for perm0 in (Sx(dom.perm)*Sx(perm)).keys()) > n:
-                continue
-            result = {}
-            for perm2 in perms:
-                for rc2 in RCGraph.all_rc_graphs(perm2, n-1):
-                    if rc2.is_dom_perm_yamanouchi(dom.perm, perm):
-                        result[perm2] = result.get(perm2, 0) + 1
+            for rc1 in RCGraph.all_rc_graphs(perm1, n-1):
+                for perm2 in perms:
+                    if not perm2.bruhat_leq(perm):
+                        continue
+                    if perm2.inv == 0:
+                        continue
+                    if max(len(perm0) for perm0 in (Sx(perm1)*Sx(perm2)).keys()) > n:
+                        continue
+                    
+                    
+                    for rc2 in RCGraph.all_rc_graphs(perm2, n-1):
+                        if tuple(a + b for a, b in zip(rc1.length_vector, rc2.length_vector)) != RCGraph.principal_rc(perm,n-1).length_vector:
+                                continue
+                        if rc1.is_dom_perm_yamanouchi(dom.perm, perm_top) and rc2.is_dom_perm_yamanouchi(dom.perm, perm_top) and tuple([a + b for a, b in zip(rc1.length_vector, rc2.length_vector)]) == RCGraph.principal_rc(perm_top,n-1).length_vector:
+                            result[(perm1,perm2)] = result.get((perm1, perm2), 0) + 1
             
-            matches = {}
+        matches = {}
 
-            for k in perms:
-                if k.inv == 0:
-                    continue         
-                product = (Sx(dom.perm) * Sx(k))
-                if max(len(perm0) for perm0 in product.keys()) > n:
-                    continue
-                if product.get(perm, 0) == result.get(k, 0):
-                    print("good")
-                    matches[k] = True
-                else:
-                    matches[k] = False
-                    print(f"Warning: mismatch! {k}: expected {product.get(perm, 0)}, got {result.get(k, 0)}")
-                    print("Distinct rcs:")
-                    for tab in tabs.get(k, []):
-                        pretty_print(tab)
-                        print(f"{(~tab.perm)=}")
-                        print("Rectified:")
-                        pretty_print(tab.rectify())
-                        print(f"{~(tab.rectify().perm)=}")
-                    print("Highest weight rcs:")
-                    for tab in components.get(k, []):
-                        print("hw_rc")
-                        pretty_print(tab)
-                        print("p_tableau")
-                        pretty_print(tab.p_tableau)
-                        print("q_tableau")
-                        pretty_print(tab.q_tableau)
-                        print("weight_tableau")
-                        pretty_print(tab.weight_tableau)
-                        print("highest_weight_tableau")
-                        pretty_print(tab.weight_tableau.to_highest_weight(length=tab.crystal_length())[0])
-                    for wt in w_tabs.get(k, []):
-                        print("lw_rc")
-                        pretty_print(wt)
-                        print("p_tableau")
-                        pretty_print(wt.p_tableau)
-                        print("q_tableau")
-                        pretty_print(wt.q_tableau)
-                        print("weight_tableau")
-                        pretty_print(wt.weight_tableau)
-                        print("highest_weight_tableau")
-                        pretty_print(wt.weight_tableau.to_highest_weight(length=wt.crystal_length())[0])
-                        # pretty_print(tab)
-                        # pretty_print(tab.rectify())
-                    input()
-                #print(f"Matches: {matches}")
+        for (perm1, perm2) in itertools.product(perms, perms):
+            product = (Sx(perm1) * Sx(perm2))
+            if max(len(perm0) for perm0 in product.keys()) > n:
+                continue
+            if product.get((perm1, perm2), 0) == result.get((perm1, perm2), 0):
+                print("good")
+                matches[(perm1, perm2)] = True
+            else:
+                matches[(perm1, perm2)] = False
+                print(f"Warning: mismatch! {(perm1, perm2)}: expected {product.get((perm1, perm2), 0)}, got {result.get((perm1, perm2), 0)}")
                 
-                if any(not v for v in matches.values()):
-                    print("Mismatch found!")
+                #input()
+            #print(f"Matches: {matches}")
+            
+            
