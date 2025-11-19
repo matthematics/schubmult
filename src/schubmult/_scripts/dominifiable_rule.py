60eebe5d55171a17993e59db30a2b80fa99ff609
Matt Samuel <matthematics@gmail.com>
2025-11-19 16:47:03 -0500

    Dominant is a module action on RCs

--- src/schubmult/_scripts/dominifiable_rule.py
+++ src/schubmult/_scripts/dominifiable_rule.py
@@ -714,26 +714,29 @@ def recursive_try_product(u_rc, v_rc, ring):
         return ring(u_rc)
     if v_rc.perm.minimal_dominant_above() == v_rc.perm:
         return ring(u_rc) % ring(v_rc)
-    elif u_rc.perm.minimal_dominant_above() == u_rc.perm:
-        return ring(v_rc) % ring(u_rc)
     
-    mid = len(u_rc) // 2
     cheat_prod = Sx(v_rc.perm) * Sx(u_rc.perm)
+    if len(u_rc) == 1:
+        result = ring.zero
+        for rc in cheat_prod:
+            return rc_ring(rc)
+    
     # if mid_u < mid_v:
     #     mid_v = mid_u
     # elif mid_v < mid_u:
     #     mid_u = mid_v
     # if mid_u == len(u_rc):
     #     raise NotImplementedError("Cannot split further")
-    left_u, right_u = u_rc.vertical_cut(mid)
-    left_v, right_v = v_rc.vertical_cut(mid)
+    left_u, right_u = u_rc.vertical_cut(len(u_rc) - 1)
+    left_v, right_v = v_rc.vertical_cut(len(u_rc) - 1)
     left_prod = recursive_try_product(left_u, left_v, ring)
     right_prod = recursive_try_product(right_u, right_v, ring)
 
     combine_attempt = left_prod * right_prod
     result = ring.zero
     for rc in combine_attempt.keys():
-        if rc.vertical_cut(mid) == (next(iter(left_prod.keys())), next(iter(right_prod.keys()))) and rc.perm in cheat_prod:
+        good = True
+        if rc.vertical_cut(len(u_rc) - 1) == (next(iter(left_prod.keys())), next(iter(right_prod.keys()))) and rc.perm in cheat_prod:
             result += rc_ring(rc)
             break
     # total_prod = ring.zero
@@ -762,7 +765,7 @@ if __name__ == "__main__":
     n = int(sys.argv[1])
 
     perms = Permutation.all_permutations(n)
-    # dom_perms = {perm.minimal_dominant_above() for perm in perms}
+    dom_perms = {perm.minimal_dominant_above() for perm in perms}
     # THIS WORKS NOTE
     # for dom in dom_perms:
     #     for v in perms:
@@ -798,39 +801,48 @@ if __name__ == "__main__":
     from schubmult import RCGraphRing
     rc_ring = RCGraphRing()
    
-    # BRANCH TEST IF CAN REPRESENT WITH ELEMNT SYMS BY PRODUCT
-    for u in perms:
-        for v in perms:
-            cheat_prod = Sx(v) * Sx(u)
-            # CRYSTAL MONK
-            # dominifiable cauchy
-            # if dominifiable(u, v):
-            #     print(f"\n=== Testing dominifiable pair u={u.trimcode}, v={v.trimcode} ===")
+    # Test module action
+    for w in perms:
+        for dom1, dom2 in itertools.product(dom_perms, dom_perms):
+            dom1_rc = RCGraph.principal_rc(dom1, n-1)
+            dom2_rc = RCGraph.principal_rc(dom2, n-1)
+
+            for w_rc in RCGraph.all_rc_graphs(w, n-1):
+                res1 =  rc_ring(w_rc) % (rc_ring(dom1_rc) % rc_ring(dom2_rc))
+                res2 = (rc_ring(w_rc) % rc_ring(dom1_rc)) % rc_ring(dom2_rc)
+                diff = res1 - res2
+                assert all(coeff == 0 for coeff in diff.values())
+            # cheat_prod = Sx(v) * Sx(u)
+            # # CRYSTAL MONK
+            # # dominifiable cauchy
+            # # if dominifiable(u, v):
+            # #     print(f"\n=== Testing dominifiable pair u={u.trimcode}, v={v.trimcode} ===")
+            # # result = rc_ring.zero
+            # # dom_rc = RCGraph.principal_rc(v.minimal_dominant_above(), n-1)
             # result = rc_ring.zero
-            # dom_rc = RCGraph.principal_rc(v.minimal_dominant_above(), n-1)
-            result = rc_ring.zero
-            for u_rc in RCGraph.all_rc_graphs(u, n-1):
-                for v_rc in RCGraph.all_rc_graphs(v, n-1):
-                    # print("ATTEMPT")
-                    # pretty_print(CrystalGraphTensor(u_rc, v_rc))
-                    result += recursive_try_product(u_rc, v_rc, rc_ring)
-                    # except Exception as e:
-                    #     import traceback
-                    #     print("FAILED TO COMPUTE PRODUCT:")
-                    #     traceback.print_exc(file=sys.stdout)
-            #assert 
-                        #print(f"FAILED TO COMPUTE PRODUCT: {e}")
-            #         if v_rc.perm.minimal_dominant_above() != v_rc.perm:
-            #             continue
-            #         dom_rc = v_rc
-            #     result += (rc_ring(u_rc) % rc_ring(dom_rc)).divdiff_perm((~v)*dom_rc.perm)
-            #         # diff_elem = (~v)*v.minimal_dominant_above()
-            #         # final_prod = prod.divdiff_perm(diff_elem)
-            #         # cheat_prod = Sx(v) * Sx(u)
-            #         # total_count = sum(cheat_prod[w] for w in final_prod.keys())
-            #         # print(f"  u_rc perm={u_rc.perm.trimcode}, v_rc perm={v_rc.perm.trimcode} => count={total_count}")
+            # for u_rc in RCGraph.all_rc_graphs(u, n-1):
+            #     for v_rc in RCGraph.all_rc_graphs(v, n-1):
+            #         # print("ATTEMPT")
+            #         # pretty_print(CrystalGraphTensor(u_rc, v_rc))
+            #         result += recursive_try_product(u_rc, v_rc, rc_ring)
+            #         # except Exception as e:
+            #         #     import traceback
+            #         #     print("FAILED TO COMPUTE PRODUCT:")
+            #         #     traceback.print_exc(file=sys.stdout)
+            # #assert 
+            #             #print(f"FAILED TO COMPUTE PRODUCT: {e}")
+            # #         if v_rc.perm.minimal_dominant_above() != v_rc.perm:
+            # #             continue
+            # #         dom_rc = v_rc
+            # #     result += (rc_ring(u_rc) % rc_ring(dom_rc)).divdiff_perm((~v)*dom_rc.perm)
+            # #         # diff_elem = (~v)*v.minimal_dominant_above()
+            # #         # final_prod = prod.divdiff_perm(diff_elem)
+            # #         # cheat_prod = Sx(v) * Sx(u)
+            # #         # total_count = sum(cheat_prod[w] for w in final_prod.keys())
+            # #         # print(f"  u_rc perm={u_rc.perm.trimcode}, v_rc perm={v_rc.perm.trimcode} => count={total_count}")
+            # # pretty_print(result)
+            # test_prod = Sx(sum([coeff * rc.polyvalue(Sx.genset) for rc, coeff in result.items()]))
+            # assert (test_prod - cheat_prod).expand() == S.Zero
+            # print(f"Successfully computed product for u={u.trimcode}, v={v.trimcode}")
             # pretty_print(result)
-            test_prod = Sx(sum([coeff * rc.polyvalue(Sx.genset) for rc, coeff in result.items()]))
-            assert (test_prod - cheat_prod).expand() == S.Zero
-            print(f"Successfully computed product for u={u.trimcode}, v={v.trimcode}")
-            pretty_print(result)
+            # print(f"Comparison: {cheat_prod}")
