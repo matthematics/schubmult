6f6559468e7115d3b589d7cef9da9131c87f3d87
Matt Samuel <matthematics@gmail.com>
2026-01-14 14:09:56 -0500

    Mult fix

--- src/schubmult/mult/__init__.py
+++ src/schubmult/mult/__init__.py
@@ -9,20 +9,21 @@ in various settings:
 - schubmult_q: Quantum Schubert polynomial multiplication
 - schubmult_q_double: Quantum double Schubert polynomial multiplication
 
-Also includes positivity display utilities for root-based representations.
+Also includes positivity utilities (posify, compute_positive_rep) for root-based representations.
 """
 
 from schubmult.mult.double import mult_poly_double, schubmult_double
-from schubmult.mult.positivity import display_positive, factor_out_q
+from schubmult.mult.positivity import compute_positive_rep, posify
 from schubmult.mult.quantum import schubmult_q
-from schubmult.mult.quantum_double import schubmult_q_double
+from schubmult.mult.quantum_double import factor_out_q, schubmult_q_double
 from schubmult.mult.single import mult_poly_py, schubmult_py
 
 __all__ = [
-    "display_positive",
+    "compute_positive_rep",
     "factor_out_q",
     "mult_poly_double",
     "mult_poly_py",
+    "posify",
     "schubmult_double",
     "schubmult_py",
     "schubmult_q",
