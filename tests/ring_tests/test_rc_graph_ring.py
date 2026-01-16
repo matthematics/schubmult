119f56b1775f32e64f9f4434fb0b946a248a9398
Matt Samuel <matthematics@gmail.com>
2026-01-16 15:03:48 -0500

    Product agrees with free

--- tests/ring_tests/test_rc_graph_ring.py
+++ tests/ring_tests/test_rc_graph_ring.py
@@ -27,6 +27,23 @@ def test_squash_product():
                         build_product[(g_rc.perm, rc2.perm)] = build_product.get((g_rc.perm, rc2.perm), R.zero) + coeff * R(rc)
                 
         for (g_perm, rc_perm), coeff in build_product.items():
-            prod = Sx(g_perm) * Sx(rc_perm)
+            prod = Sx(g_perm) * Sx(perm)
             assert all([prod.get(rc_result.perm, 0) == c for rc_result, c in coeff.items()])
     
+def test_agrees_with_free_algebra():
+    from schubmult import RCGraph, RCGraphRing, ASx, Permutation
+    import itertools
+    n = 4
+    
+    perms = Permutation.all_permutations(n)
+
+    for perm1, perm2 in itertools.product(perms, repeat=2):
+        for len1 in range(len(perm1.trimcode), n):
+            for len2 in range(len(perm2.trimcode), n):
+                for rc1, rc2 in itertools.product(RCGraph.all_rc_graphs(perm1, len1), RCGraph.all_rc_graphs(perm2, len2)):
+                    R = RCGraphRing()
+                    rc_elem = R(rc1) * R(rc2)
+
+                    free_algebra_elem = ASx(rc1.perm, len(rc1)) * ASx(rc2.perm, len(rc2))
+                    free_elem_test = rc_elem.to_free_algebra_element()
+                    assert all(v == 0 for v in (free_algebra_elem - free_elem_test).values())
