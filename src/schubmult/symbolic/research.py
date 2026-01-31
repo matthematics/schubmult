e769b21ccdb6dd2b1672996a4bdfaf8f2bdd9865
Matt Samuel <matthematics@gmail.com>
2026-01-30 20:55:04 -0500

    EMPTY file

--- src/schubmult/symbolic/research.py
+++
@@ -1,8 +0,0 @@
-# metaclass
-# symengine and sympy together but otherwise itself
-import symengine.lib.symengine_wrapper as sw
-import sympy
-
-
-class Bacon(sympy.Expr, metaclass=sw.BasicMeta):
-    pass
