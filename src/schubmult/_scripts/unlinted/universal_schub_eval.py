b72428a214adc97d52ce0ed21bb46d11cc4ffd43
Matt Samuel <matthematics@gmail.com>
2026-02-28 19:41:15 -0500

    Universal schub confirmed

--- src/schubmult/_scripts/unlinted/universal_schub_eval.py
+++ src/schubmult/_scripts/unlinted/universal_schub_eval.py
@@ -17,13 +17,13 @@ if __name__ == "__main__":
         for word in dual_schub:
             m = max([a + i for i, a in enumerate(word)])
             if m > mx:
-                mx = n
-        mx = n - 1
+                mx = m
+        #mx = n
         for word, coeff in dual_schub.items():
             elem_prod = S.One
             for i, a in enumerate(word):
-                elem_prod *= E(mx - i - a, mx - i, x, [0 for _ in range(3*n)])
-            sm += coeff * expand(elem_prod, func=True)
+                elem_prod *= E(mx - i - a, mx - i, x[1:], [0 for _ in range(3*n)])
+            sm += coeff * elem_prod
         schub_elem = Sx.from_expr(sm)
         assert all(coeff >= 0 for coeff in schub_elem.values()), f"Failed for {perm}, got {schub_elem}"
         assert len({k: v for k, v in schub_elem.items() if v != 0}) == 1, f"Failed for {perm}, got {schub_elem}"
