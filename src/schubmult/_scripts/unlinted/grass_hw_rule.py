60773a24588e572950b93ac6e97eae24c4abe8ca
Matt Samuel <matthematics@gmail.com>
2026-04-06 20:11:59 -0400

    Highest weights galore

---
+++ src/schubmult/_scripts/unlinted/grass_hw_rule.py
@@ -0,0 +1,294 @@
+from multiprocessing import Pool, cpu_count
+from schubmult.rings.polynomial_algebra import *
+from schubmult import *
+from schubmult.abc import x
+from schubmult.utils.perm_utils import mu_A
+from schubmult.rings.free_algebra import *
+from schubmult.utils.tuple_utils import pad_tuple
+from schubmult.combinatorics.permutation import bad_classical_patterns
+import itertools
+
+def check_perm(perm_arr):
+    perm = Permutation(perm_arr)
+    n = len(perm_arr)
+    r = RCGraphRing()
+    for i in range(len(perm.trimcode), n):
+        rc_elem = r.from_free_algebra_element(ASx(perm, i))
+        if not all(int(v) in {1, 0, -1} for v in rc_elem.values()):
+            return (False, perm_arr, dict(rc_elem))
+    return (True, perm_arr, None)
+
+EP = PolynomialAlgebra(ElemSymPolyBasis(Sx.genset))
+
+# def in_good_algebra(elem_comp, monomial_vec):
+#     #monom = EP.from_dict({elem_comp: 1}).change_basis(EP._basis.monomial_basis)
+#     #return monom.get(monomial_vec, 0) != 0
+#     assert isinstance(elem_comp[1], Permutation)
+#     if FA(*)
+#     return True
+
+SS = FreeAlgebra(SchubertSchurBasis)
+EE = FreeAlgebra(ElementaryBasis)
+
+
+neg_pats = [Permutation([1, 4, 3, 2]), Permutation([4, 1, 3, 2]), Permutation([3, 1, 4, 2])]
+
+
+g = GrassTensorAlgebra()
+r = RCGraphRing()
+def grass_tensor_elem(perm, n = None):
+    if n is None:
+        n = len(perm.trimcode)
+    grass_elem = ring.zero
+    cem = RCGraph.full_CEM(perm, n)
+    
+    for rc, cem_dict in cem.items():
+        #if len(rc.perm) <= n:
+        if True:
+            grass_elem += g.from_dict(cem_dict)
+        elif rc.perm != perm:
+            grass_elem += g.from_dict(cem_dict)
+        else:
+            rc_base, rc_grass = rc.resize(n).squash_decomp()
+            if rc_grass.perm.inv != 0:
+                grass_elem += g.from_dict({(*k, rc_grass): v for k, v in RCGraph.full_CEM(rc_base.perm, n - 1)[rc_base.resize(n - 1)].items()})
+            else:
+                grass_elem += g.from_dict(cem_dict)
+    return grass_elem
+
+def n_tensor(key, n):
+    factor1 = g.one
+    factor2 = g.one
+    flag = False
+    for index in range(len(key)):
+        if len(key[index].perm) <= n:
+            factor1 *= g((key[index],))
+        else:
+            factor2 *= g((key[index],))
+
+    return factor1 @ factor2
+
+def doit(grass_elem1, n):
+    picnic1 = (g@r).zero
+    for key, coeff in grass_elem1.items():
+        to_add1 = g.one
+        index = 0
+        for rc in key:
+            if len(rc.perm.trimcode) < n - 1:
+                to_add1 *= g((rc,))
+                index += 1
+            else:
+                break
+        assert (next(iter([len(rc.perm) for rc in to_add1.to_rc_graph_ring_element()]))) <= n - 1
+        picnic1 += coeff * to_add1 @ g(key[index:]).to_rc_graph_ring_element().resize(n-1)
+    return picnic1
+
+def cem_schub(perm, n):
+    return sum([g.from_dict(cem_dict) for rc, cem_dict in RCGraph.full_CEM(perm, n).items()])
+
+def cem_schub_schur_decomp(perm, n):
+    from sympy import Mul, Add, expand
+    result = Sx.zero @ Sx.zero
+    cd = perm.strict_mul_dominant().trimcode
+    if any(a < n for a in cd):
+        toadd = min(n - a for a in cd if a < n)
+        cd = [a + toadd for a in cd]
+    domperm = uncode(cd)
+    reppy = expand(Sx(perm).cem_rep(mumu=~domperm, elem_func=Sx.symbol_elem_func), func=False)
+    for arg in Add.make_args(reppy):
+        coeff, schur_part = arg.as_coeff_Mul()
+        part1 = Sx.one
+        part2 = Sx.one
+        for elem_arg in Mul.make_args(schur_part):
+            if elem_arg.numvars < n:
+                part1 *= elem_arg
+            else:
+                part2 *= elem_arg
+        result += coeff * part1 @ part2
+    return result
+
+def schub_schur_decomp(perm, n):
+    result = r.schub(perm, n)
+    if len(perm) <= n:
+        return result @ r.one.resize(n)
+    factorization = (r@r).zero
+    seen = set()
+    while not result.almosteq(r.zero):
+        itemlist = list(sorted(result.items()))
+        
+        for rc, coeff in itemlist:
+            if coeff == 0:
+                continue
+            if len(rc.perm) <= n:
+                if (rc.perm, Permutation([])) in seen:
+                    continue
+                seen.add((rc.perm, Permutation([])))
+                factorization += coeff * cem_schub(rc.perm, n).to_rc_graph_ring_element().resize(n) @ r.one.resize(n)
+                result -= coeff * cem_schub(rc.perm, n).to_rc_graph_ring_element().resize(n)                
+            elif rc.perm.descents() == {n-1}:
+                if (Permutation([]), rc.perm) in seen:
+                    continue
+                seen.add((Permutation([]), rc.perm))
+                factorization += coeff * r.one.resize(n) @ cem_schub(rc.perm, n).to_rc_graph_ring_element().resize(n)
+                result -= coeff * cem_schub(rc.perm, n).to_rc_graph_ring_element().resize(n)
+            else:
+                rc_base, rc_grass = rc.resize(n).squash_decomp()
+                if (rc_base.perm, rc_grass.perm) in seen:
+                    continue
+                seen.add((rc_base.perm, rc_grass.perm))
+                blast_element = cem_schub(rc_base.perm, n) * cem_schub(rc_grass.perm, n)
+                result -= coeff * blast_element.to_rc_graph_ring_element().resize(n)
+                factorization += coeff * cem_schub(rc_base.perm, n).to_rc_graph_ring_element().resize(n) @ cem_schub(rc_grass.perm, n).to_rc_graph_ring_element().resize(n)
+                print("REsult after processing", rc.perm, "is", result)
+                pretty_print(result)
+    return factorization
+
+
+if __name__ == "__main__":
+    from schubmult.combinatorics.crystal_graph import CrystalGraphTensor
+    from schubmult.abc import *
+    from schubmult.rings.polynomial_algebra import *
+    from sympy import pretty_print
+    import sys
+    import itertools
+    n = int(sys.argv[1])
+    extra = int(sys.argv[2]) if len(sys.argv) > 2 else 0
+    perms = [perm for perm in Permutation.all_permutations(n + extra) if len(perm.trimcode) <= n]
+    
+    # for perm in perms:
+    #     if len(perm) > n:
+    #         print("Trying perm", perm)
+    #         print(cem_schub_schur_decomp(perm, n))
+    #sys.exit(0)
+    for perm1, perm2 in itertools.product(perms, repeat=2):
+        if perm1.inv == 0 or perm2.inv == 0:
+            continue
+        cem1 = RCGraph.full_CEM(perm1, n + 1)
+        cem2 = RCGraph.full_CEM(perm2, n + 1)
+        # cem_elem1 = cem_schub_schur_decomp(perm1, n)
+        # cem_elem2 = cem_schub_schur_decomp(perm2, n)
+
+        test_elem = Sx.zero
+
+        # for (a1, a2), coeff in cem_elem1.items():
+        #     test_elem += coeff * Sx(a1) * Sx(a2)
+        # assert test_elem == Sx(perm1)
+        # test_elem2 = Sx.zero
+        # for (a1, a2), coeff in cem_elem2.items():
+        #     test_elem2 += coeff * Sx(a1) * Sx(a2)
+        # assert test_elem2 == Sx(perm2)
+
+        # ghost1 = {rc: cem1[rc] for rc in cem1 if rc.perm != perm1}
+        # ghost2 = {rc: cem2[rc] for rc in cem2 if rc.perm != perm2}
+
+        # non_ghost1 = {rc: cem1[rc] for rc in cem1 if rc.perm == perm1}
+        # non_ghost2 = {rc: cem2[rc] for rc in cem2 if rc.perm == perm2}
+
+        # nontrivial_ghost1 = [((g.from_dict(g_cem1) * g.from_dict(elem2)).to_rc_graph_ring_element())  for g_cem1, elem2 in itertools.product(ghost1.values(), non_ghost2.values()) if not (g.from_dict(g_cem1) * g.from_dict(elem2)).to_rc_graph_ring_element().almosteq(r.zero)]
+        # nontrivial_ghost2 = [((g.from_dict(elem1) * g.from_dict(g_cem2)).to_rc_graph_ring_element())  for g_cem2, elem1 in itertools.product(ghost2.values(), non_ghost1.values()) if not (g.from_dict(elem1) * g.from_dict(g_cem2)).to_rc_graph_ring_element().almosteq(r.zero)]
+        # ghost_positive = r.zero
+        # ghost_negative = r.zero
+        # for ghost_elem in nontrivial_ghost1 + nontrivial_ghost2:
+        #     for rc, coeff in ghost_elem.items():
+        #         if coeff > 0:
+        #             ghost_positive += coeff * r(rc)
+        #         else:
+        #             ghost_negative += -coeff * r(rc)
+        
+        #result = r.zero
+        result = (r@r).zero
+        prd = Sx(perm1) * Sx(perm2)
+        for rc1, cem_dict1 in cem1.items():
+            for rc2, cem_dict2 in cem2.items():
+                rc01 = doit(g.from_dict(cem_dict1), n + 1)
+                rc02 = doit(g.from_dict(cem_dict2), n + 1)
+                for (base_rc1, grass_rc1), coeff1 in rc01.items():
+                    for (base_rc2, grass_rc2), coeff2 in rc02.items():
+                        new_base_elem = (g(base_rc1)*g(base_rc2)).to_rc_graph_ring_element().resize(n)
+                        for new_base_rc, coeff0 in new_base_elem.items():
+                            if new_base_rc.is_highest_weight:
+                                new_elem = new_base_rc.squash_product(grass_rc1.resize(n)).squash_product(grass_rc2.resize(n))
+                                if new_elem.is_highest_weight:
+                                    elem0, grass_rc0 = new_elem.squash_decomp()
+                                    result += coeff1 * coeff2 * coeff0 * r(elem0) @ r(grass_rc0)
+                    
+                    # if rc.is_highest_weight:
+                    #     result += coeff1 * coeff2 * coeff3 * (r@r)(rc.squash_decomp())
+                # cem2 = RCGraph.full_CEM(base_perm2, n)
+                # for rc1, cem_dict1 in cem1.items():
+                #     for rc2, cem_dict2 in cem2.items():
+                #         for key1, coeff11 in cem_dict1.items():
+                #             for key2, coeff22 in cem_dict2.items():
+                #                 elem = next(iter((g(key1) * g(key2)).to_rc_graph_ring_element().resize(n)))
+                #                 #base_graph, grass1 = elem.squash_decomp()
+                #                 for grass_rc1 in RCGraph.all_rc_graphs(grass_perm, n):
+                #                     for grass_rc2 in RCGraph.all_rc_graphs(grass_perm2, n):
+                #                         new_elem = elem.squash_product(grass_rc1).squash_product(grass_rc2)
+                #                         if new_elem.is_highest_weight:
+                #                             elem0, grass_rc0 = new_elem.squash_decomp()
+                #                             result += coeff1 * coeff2 * coeff11 * coeff22 * r(elem0) @ r(grass_rc0)
+                        # for up_grass_perm, coeff3 in up_grass.items():
+                        #     grass_cem = RCGraph.full_CEM(up_grass_perm, n)
+                        #     for rcc, grass_cem_dict in grass_cem.items():
+                        #         try:
+                        #             grass_rc = next(iter(g.from_dict(grass_cem_dict).to_rc_graph_ring_element().resize(n)))
+                        #         except StopIteration:
+                        #             continue
+                        #         new_elem = elem.squash_product(grass_rc)
+                        #         if new_elem.is_highest_weight:
+                        #             elem0, grass_rc0 = new_elem.squash_decomp()
+                        #             result += coeff1 * coeff2 * coeff3 * r(elem0) @ r(grass_rc0)
+                # if (rc1.perm != perm1 or rc2.perm != perm2) and not (rc1.perm == perm1 and rc2.perm == perm2):
+                #     ghost_elem = (g.from_dict(cem_dict1) * g.from_dict(cem_dict2)).to_rc_graph_ring_element()
+                #     for rc, coeff in ghost_elem.items():
+                #         if coeff > 0:
+                #             ghost_positive -= coeff * r(rc)
+                #         else:
+                #             ghost_negative -= -coeff * r(rc)
+                #     continue
+                # for (f11, f12), v1 in cem_tensor1.items():
+                #     for (f21, f22), v2 in cem_tensor2.items():
+                #         # first_result = next(iter((g(f11) * g(f21)).to_rc_graph_ring_element())).resize(n)
+                #         # tensor = CrystalGraphTensor(first_result.resize(n),f12.resize(n).squash_product(f22.resize(n)))
+                #         # tensor_hw, raise_seq = tensor.to_highest_weight()
+                #         #tensor = CrystalGraphTensor(tensor_hw, f12.resize(n).squash_product(f22.resize(n)))
+                #         #result += v1 * v2 * r(tensor.factors[0]) squash_product(tensor.factors[1]).reverse_raise_seq(raise_seq))
+                #         #result += v1 * v2 * r(tensor.factors[0]) @ r(tensor.factors[1]).reverse_raise_seq(raise_seq)
+                #         # if tensor.is_highest_weight:
+                #         #     result += v1 * v2 * r(tensor.factors[0]) @ r(tensor.factors[1])
+                #         rcc = next(iter(((g(f11) * g(f21))*(g((f12,))*g((f22,)))).to_rc_graph_ring_element().resize(n)))
+                #         if rcc.is_highest_weight:
+                #             result += v1 * v2 * (r@r)(rcc.squash_decomp())
+                        # the_base_elem = doit(g(f11) * g(f21), n)
+                        # for (rc11, rc22), coeff2 in the_base_elem.items():
+                        #     grass_rc = next(iter((g((rc22,))*g((f12,))*g((f22,))).to_rc_graph_ring_element())).resize(n-1)
+                        #     assert grass_rc.perm.inv == 0 or grass_rc.perm.descents() == {n-2}, f"Expected grass_rc to have at most one descent, got {grass_rc.perm.descents()} for {perm1}, {perm2} at {rc1.perm}, {rc2.perm}"
+                        #     #assert len(grass_rc.perm.descents()) == 1, f"Expected grass_rc to have exactly one descent, got {grass_rc.perm.descents()} for {perm1}, {perm2} at {rc1.perm}, {rc2.perm}"
+                        #     #if rc11.is_highest_weight and 
+                        #     #if rc11.is_highest_weight:
+                        #         # GOOD
+                        #         #result += v1 * v2 * coeff2 * (g(rc11)*g((rc22,))*g((f12,))*g((f22,))).to_rc_graph_ring_element()#.to_rc_graph_ring_element() @ r(grass_rc)
+                        #     result += v1 * v2 * coeff2 * (g(rc11).to_rc_graph_ring_element())@(g((rc22,))*g((f12,))*g((f22,))).to_rc_graph_ring_element()#.to_rc_graph_ring_element() @ r(grass_rc)
+                        # tensor = CrystalGraphTensor(first_result.resize(n),f12.resize(n).squash_product(f22.resize(n)))
+                        # if tensor.factors[0].is_highest_weight:
+                        #     #result += v1 * v2 * r(tensor.factors[0].squash_product(tensor.factors[1]))
+                        #     result += v1 * v2 * r(tensor.factors[0]) @ r(tensor.factors[1])
+                # rc_prd = (g.from_dict(cem_dict1) * g.from_dict(cem_dict2)).to_rc_graph_ring_element()
+                # # assert len(rc_prd) == 1, f"Multiple terms in product for {perm1}, {perm2} at {rc1.perm}, {rc2.perm}: {rc_prd}"
+                # # assert next(iter(rc_prd.values())) == 1, f"Coefficient not 1 in product for {perm1}, {perm2} at {rc1.perm}, {rc2.perm}: {rc_prd}"
+                # result += rc_prd
+        #assert ghost_negative.almosteq(r.zero), f"Negative ghost terms remaining for {perm1}, {perm2}: {ghost_negative}, {ghost_positive}"
+        #assert ghost_positive.almosteq(r.zero), f"Positive ghost terms remaining for {perm1}, {perm2}: {ghost_positive}, {ghost_negative}"
+        #result += ghost_positive - ghost_negative
+        prd2 = Sx.zero
+        for (rc_base, rc_grass), coeff in result.items():
+        #for rc, coeff in result.items():
+            rc = rc_base.resize(n).squash_product(rc_grass.resize(n))
+            assert coeff == prd.get(rc.perm, 0), f"Failure for {perm1}, {perm2} at {rc.perm} with coeff {coeff} vs {prd.get(rc.perm, 0)}"
+            if rc.perm not in prd2:
+                prd2 += coeff * Sx(rc.perm)
+        assert prd == prd2, f"Failure for {perm1}, {perm2} with result {result}, expected {prd}, got {prd2}"
+        print("Success for", perm1, perm2)
+
+        
+        
