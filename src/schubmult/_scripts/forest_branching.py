26405bc7ee42a7f56952d35eebc5f9d73f954412
Matt Samuel <matthematics@gmail.com>
2026-04-24 14:06:58 -0400

    Doc updated

--- src/schubmult/_scripts/forest_branching.py
+++ src/schubmult/_scripts/forest_branching.py
@@ -2,11 +2,15 @@ from schubmult import *
 from schubmult.rings.polynomial_algebra import *
 from schubmult.rings.free_algebra import *
 from schubmult.utils.tuple_utils import pad_tuple
-from schubmult.rings.combinatorial.hw_rc_ring import HWRCGraphRing
+from schubmult.rings.combinatorial.forest_rc_ring import ForestRCGraphRing
 from sympy import pretty_print
 
 def snap_key(rc):
     return next(iter([rcc for rcc in RCGraph.all_rc_graphs(rc.perm, len(rc), weight=rc.extremal_weight)]))
+
+def forest_equal(rc1, rc2):
+    return rc1.forest_weight == rc2.forest_weight and rc1.omega_invariant[1] == rc2.omega_invariant[1]
+
 
 if __name__ == "__main__":
     import sys
@@ -14,8 +18,8 @@ if __name__ == "__main__":
 
     n = int(sys.argv[1])
     perms = Permutation.all_permutations(n)
-    r = RCGraphRing()
-    Key = PolynomialAlgebra(KeyPolyBasis(Sx.genset))
+    r = ForestRCGraphRing()
+    # Forest = PolynomialAlgebra(KeyPolyBasis(Sx.genset))
     # KeyDual = FreeAlgebra(KeyBasis)
 
     # for perm in perms:
@@ -37,23 +41,24 @@ if __name__ == "__main__":
         comp1 = pad_tuple(tuple(perm1.trimcode), n - 1)
         comp2 = pad_tuple(tuple(perm2.trimcode), n - 1)
 
-        # forest_rc1 = next(iter([rc for rc in RCGraph.all_rc_graphs(perm1, n - 1, weight=comp1) if rc.forest_weight == comp1]))
-        # forest_rc2 = next(iter([rc for rc in RCGraph.all_rc_graphs(perm2, n - 1, weight=comp2) if rc.forest_weight == comp2]))
-        forest_rc1 = r.from_dict({k: v for k, v in r.from_free_algebra_element(ForestDual(*comp1)).items() if k.forest_weight == comp1})
-        forest_rc2 = r.from_dict({k: v for k, v in r.from_free_algebra_element(ForestDual(*comp2)).items() if k.forest_weight == comp2})
+        forest_rc1 = r(RCGraph.principal_rc(perm1, n - 1))
+        forest_rc2 = r(RCGraph.principal_rc(perm2, n - 1))
+        # forest_rc1 = r.from_free_algebra_element(ForestDual(*comp1))
+        # forest_rc2 = r.from_free_algebra_element(ForestDual(*comp2))
+        # forest_rc1 = r(next(iter([rc for rc in RCGraph.all_rc_graphs(perm1, len(comp1)) if rc.forest_weight == comp1])))
+        # forest_rc2 = r(next(iter([rc for rc in RCGraph.all_rc_graphs(perm2, len(comp2)) if rc.forest_weight == comp2])))
 
         rc_test_prod = ForestDual.zero
-        invariants_seen = {}
+        #invariants_seen = {}
         rc_prod = (forest_rc1 * forest_rc2)
         # pretty_print(rc_prod)
         # print({k.forest_invariant: v for k, v in rc_prod.items()})
         for k, v in rc_prod.items():
-            if k.forest_weight not in invariants_seen:
-                invariants_seen[k.forest_weight] = k.forest_invariant
-            elif invariants_seen[k.forest_weight] != k.forest_invariant:
-                continue
+            if k.forest_weight != pad_tuple(k.perm.trimcode, len(k)):
+                raise ValueError(f"Failed for {comp1} * {comp2}, got non-forest RC graph {k} with weight {k.forest_weight} and perm {k.perm}, expected only forest RC graphs")
             rc_test_prod += v * ForestDual(*k.forest_weight)
         #rc_test_prd = sum([v * ForestDual(*k.forest_weight) for k, v in (r(forest_rc1) * r(forest_rc2)).items() if k.is_principal])
+        #rc_test_prod = rc_prod.to_free_algebra_element(ForestBasis)
         expected_prd = ForestDual(*comp1) * ForestDual(*comp2)
         assert rc_test_prod.almosteq(expected_prd), f"Failed for {comp1} * {comp2}, got {rc_test_prod} but expected {expected_prd}"
         print(f"Passed for {comp1} * {comp2}")
---
+++ src/schubmult/rings/combinatorial/forest_rc_ring.py
@@ -0,0 +1,59 @@
+from schubmult.combinatorics.rc_graph import RCGraph
+
+from .rc_graph_ring import RCGraphRing, RCGraphRingElement
+
+
+def _canonical_rc(rc):
+    from schubmult.utils.tuple_utils import pad_tuple
+
+    if rc.forest_weight != pad_tuple(tuple(rc.perm.trimcode), len(rc)):
+        return None
+    perm = rc.perm
+    the_set = {rc2 for rc2 in RCGraph.all_rc_graphs(perm, len(rc), weight=rc.length_vector) if rc2.omega_invariant[1] == rc.omega_invariant[1] and rc2.forest_weight == rc.forest_weight and rc2.forest_weight == pad_tuple(tuple(rc.perm.trimcode), len(rc))}
+    return next(iter(the_set))
+
+class ForestRCGraphRingElement(RCGraphRingElement):
+    pass
+
+class ForestRCGraphRing(RCGraphRing):
+    _id = 0
+
+    def __init__(self, *_, **__):
+        self._ID = ForestRCGraphRing._id
+        ForestRCGraphRing._id += 1
+        self.dtype = type("ForestRCGraphRingElement", (ForestRCGraphRingElement,), {"ring": self})
+
+    def __hash__(self):
+        return hash(("Dinkbeasfrrtaffffsfsfystoa", self._ID))
+
+    def new(self, x):
+        return self.from_dict({x: 1}, snap=True)
+
+    def __call__(self, x):
+        return self.new(x)
+
+    @property
+    def zero_monom(self):
+        return RCGraph([])
+
+    def _snap(self, elem):
+        ret = self.zero
+        for key, coeff in elem.items():
+            the_rc = _canonical_rc(key)
+            if the_rc is not None:
+                ret += self.from_dict({the_rc: coeff})
+        return ret
+
+    def mul(self, a, b):
+        return self.from_dict(super().mul(a, b), snap=True)
+
+    def rmul(self, a, b):
+        return self.from_dict(super().rmul(a, b), snap=True)
+
+    def from_dict(self, dct, snap=False):
+        elem = super().from_dict(dct)
+        #elem.ring = self
+        if snap:
+            elem = self._snap(elem)
+        #elem.ring = self
+        return self.dtype({k: v for k, v in elem.items() if v != 0})
