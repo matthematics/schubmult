73c55c9885a7df90d051602ebf6fe5497e51996a
Matt Samuel <matthematics@gmail.com>
2026-04-26 11:46:48 -0500

    Forest elem sym prod

---
+++ src/schubmult/_scripts/forest_prod.py
@@ -0,0 +1,57 @@
+from schubmult import *
+from schubmult.rings.polynomial_algebra import *
+from schubmult.rings.free_algebra import *
+from schubmult.rings.combinatorial.forest_rc_ring import ForestRCGraphRing
+from sympy import pretty_print
+
+# def snap_key(rc):
+#     return next(iter([rcc for rcc in RCGraph.all_rc_graphs(rc.perm, len(rc), weight=rc.extremal_weight)]))
+
+# def forest_equal(rc1, rc2):
+#     return rc1.forest_weight == rc2.forest_weight and rc1.omega_invariant[1] == rc2.omega_invariant[1]
+
+
+if __name__ == "__main__":
+    import sys
+    import itertools
+
+    n = int(sys.argv[1])
+    perms = Permutation.all_permutations(n)
+    r = ForestRCGraphRing()
+    # Forest = PolynomialAlgebra(KeyPolyBasis(Sx.genset))
+    # KeyDual = FreeAlgebra(KeyBasis)
+
+    # for perm in perms:
+    #     if perm.inv == 0:
+    #         continue
+    #     comp = pad_tuple(tuple(perm.trimcode), n - 1)
+    #     forest_set = [rc for rc in RCGraph.all_rc_graphs(perm, n - 1) if rc.forest_weight == comp]
+
+    #     for index in range(1, n - 1):
+    #         fbranch = Forest(*comp).branch(index)
+    #         tbranch = (Forest @ Forest).zero
+    #         for rc in forest_set:
+    #             rc1, rc2 = rc.vertical_cut(index)
+    #             if rc1.forest_weight == rc1.length_vector and rc2.forest_weight == rc2.length_vector:
+    #                 tbranch += Forest(*rc1.forest_weight) @ Forest(*rc2.forest_weight)
+    #         assert fbranch.almosteq(tbranch), f"Failed for {perm} at index {index} with {len(forest_set)} RC graphs, got {tbranch} but expected {fbranch}"
+
+    for perm1, perm2 in itertools.product(perms, repeat=2):
+        for length1, length2 in itertools.product(range(len(perm1.trimcode), n), range(len(perm2.trimcode), n)):
+            comp1 = perm1.pad_code(length1)
+            comp2 = perm2.pad_code(length2)
+            for forest_rc1_base, forest_rc2_base in itertools.product(RCGraph.all_forest_rcs(comp1), RCGraph.all_forest_rcs(comp2)):
+                forest_rc1 = r(forest_rc1_base)
+                forest_rc2 = r(forest_rc2_base)
+
+                rc_test_prod = ForestDual.zero
+
+                rc_prod = (forest_rc1 * forest_rc2)
+
+                for k, v in rc_prod.items():
+                    if k.forest_weight != k.perm.pad_code(len(k)):
+                        raise ValueError(f"Failed for {comp1} * {comp2}, got non-forest RC graph {k} with weight {k.forest_weight} and perm {k.perm}, expected only forest RC graphs")
+                    rc_test_prod += v * ForestDual(*k.forest_weight)
+                expected_prd = ForestDual(*comp1) * ForestDual(*comp2)
+                assert rc_test_prod.almosteq(expected_prd), f"Failed for {comp1} * {comp2}, got {rc_test_prod} but expected {expected_prd}"
+                print(f"Passed for {comp1} * {comp2}")
--- src/schubmult/_scripts/forest_schub.py
+++ src/schubmult/_scripts/forest_schub.py
@@ -1,35 +1,43 @@
-# from schubmult import *
-# from schubmult.rings.polynomial_algebra import *
-# from schubmult.rings.free_algebra import *
-# from schubmult.rings.combinatorial.forest_rc_ring import ForestRCGraphRing
-# from sympy import pretty_print
+from schubmult import *
+from schubmult.rings.polynomial_algebra import *
+from schubmult.rings.free_algebra import *
+from schubmult.rings.combinatorial.forest_rc_ring import ForestRCGraphRing
+from sympy import pretty_print
 
 
 
-# if __name__ == "__main__":
-#     import sys
-#     import itertools
+if __name__ == "__main__":
+    import sys
+    import itertools
 
-#     n = int(sys.argv[1])
-#     perms = Permutation.all_permutations(n)
+    n = int(sys.argv[1])
+    comps = [perm.pad_code(n - 1) for perm in Permutation.all_permutations(n)]
     
-#     ForestPoly = PolynomialAlgebra(ForestPolyBasis(Sx.genset))
-#     r = ForestRCGraphRing()
-#     #CompSchub = PolynomialAlgebra(CompositionSchubertBasis)
-#     length = n - 1
-#     for perm1, perm2 in itertools.product(perms, repeat=2):
-#         comp1 = perm1.pad_code(length)
-#         comp2 = perm2.pad_code(length)
-#         fschub1 = ForestPoly(*comp1)
-#         fschub2 = ForestPoly(*comp2)
-#         fprd = fschub1 * fschub2
-
-#         cschub1 = Schub(perm1, length)
-#         cschub2 = Schub(perm2, length)
-#         cprd = cschub1 * cschub2
-#         rc_reduce = r.zero
-#         for (perm, lenn), v in cprd.items():
-#             rc_reduce = 
-#         nonsense = ForestPoly.from_dict({p.pad_code(lenn): v for (p, lenn), v in cprd.items() })
-#         assert cprd.almosteq(nonsense), f"Failed for {perm1} and {perm2}, got {nonsense} but expected {fprd}"
+    ForestPoly = PolynomialAlgebra(ForestPolyBasis(Sx.genset))
+    r = ForestRCGraphRing()
+    #CompSchub = PolynomialAlgebra(CompositionSchubertBasis)
+    length = n - 1
+    for comp in comps:
+        #for k in range(1, n):
         
+        if True:
+            k = length
+            for p in range(1, k + 1):
+                acc = r.zero
+                elem_comp = tuple([0] * (k - p) + [1] * p)
+                prodo = ForestPoly(*comp) * ForestPoly(*elem_comp)
+                for forest_rc in RCGraph.all_forest_rcs(comp):
+                    for elem_rc in RCGraph.all_rc_graphs(uncode(elem_comp), length):
+                        the_squash = forest_rc.squash_product(elem_rc)
+                        forest_squash = next(iter([rc for rc in RCGraph.all_forest_rcs(the_squash.forest_weight) if rc.length_vector == the_squash.length_vector and rc.omega_invariant[1] == the_squash.omega_invariant[1]]), None)
+                        assert forest_squash.forest_weight in prodo
+                        # acc[forest_squash.forest_weight] = acc.get(forest_squash.forest_weight, set())
+                        # # if forest_squash in acc[forest_squash.forest_weight]:
+                        # #     print(f"Duplicate squash product for {forest_rc} and {elem_rc} yielding {the_squash} with forest weight {the_squash.forest_weight}")
+                        # acc[forest_squash.forest_weight].add(forest_squash)
+                        acc += r(forest_squash)
+                        #the_squash.forest_weight == the_squash.perm.pad_code(length), f"Squash product of {forest_rc} and {elem_rc} is {the_squash} and has forest weight {the_squash.forest_weight} instead of {the_squash.perm.pad_code(length)}"
+                for frc, coeff in acc.items():
+                    # if compo not in acc:
+                    #     print(f"Missing squash product for {comp} and {elem_comp} yielding {compo}")
+                    assert prodo[frc.forest_weight] == coeff, f"Coefficient mismatch for {comp} and {elem_comp} yielding {frc.forest_weight}, got {coeff} but expected {prodo[frc.forest_weight]}"
