685c87cccd155b4ff5403f050ee017615c7a7fcb
Matt Samuel <matthematics@gmail.com>
2026-07-06 19:09:50 -0400

    Fix

---
+++ src/schubmult/symbolic/common_polys.py
@@ -0,0 +1,6 @@
+from .poly import schub_poly as _schub_poly
+from .poly.schub_poly import *  # noqa: F403
+
+_vars = _schub_poly._vars
+call_zvars = _schub_poly.call_zvars
+q_vector = _schub_poly.q_vector
